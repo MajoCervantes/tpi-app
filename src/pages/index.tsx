e7b93760c6dcb1c7febb0efd4f03f18ab55e3be5
Majo <champiis49@gmail.com>
2023-06-29 13:12:06 -0600

    google fonts link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,10 @@ export default function Home() {
 				<meta name='PTI' content='PROMOTORA TECNOLÓGICA DE INVERSIONES' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
+				<link
+					rel='stylesheet'
+					href='https://fonts.googleapis.com/css2?family=Crimson+Pro'
+				/>
 			</Head>
 			<main>
 				<LoginPage />
